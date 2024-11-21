13ea2c4afe46a8dbfe54b6e04b597d16baa29edd
Kapustkin Nikita Yurievich <142444812+nikitakapustkin@users.noreply.github.com>
2024-11-21 17:05:09 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,81 +1,106 @@
 #include <iostream>
+#include <functional>
 
+struct AsNat {
+    int value;
 
+    AsNat(int v = 0) : value(v >= 0 ? v : 0) {}
 
-int z(int) {
-    return 0;
-}
-
-
-
-int n(int x) {
-    return x + 1;
-}
-
-int pred(int x) {
-    int k = 0;
-    if (x == 0) {
-        return 0;
+    AsNat operator+(const AsNat& other) const {
+        return AsNat(this->value + other.value);
     }
-    while(n(k) < x) {
-        k = n(k);
+
+    AsNat operator-(const AsNat& other) const {
+        return AsNat(this->value - other.value);
     }
-    return k;
+
+    friend std::ostream& operator<<(std::ostream& os, const AsNat& an) {
+        os << an.value;
+        return os;
+    }
+};
+
+
+AsNat z(const AsNat&) {
+    return AsNat(0);
 }
 
-int u1(int x, int) {
+
+AsNat n_func(const AsNat& x) {
+    return x + AsNat(1);
+}
+
+
+
+
+AsNat r(const AsNat& n, const AsNat& init_value,
+        std::function<AsNat(const AsNat&)> base,
+        std::function<AsNat(const AsNat&, const AsNat&)> r_step) {
+    return (n.value == 0) ? base(init_value) : r_step(init_value, r(n - AsNat(1), init_value, base, r_step));
+}
+
+
+AsNat pred(const AsNat& x) {
+    // Рекурсивное вычисление через подстановку
+    return r(x, x, z, [](const AsNat& a, const AsNat&) -> AsNat { return a - AsNat(1); });
+}
+
+
+AsNat sub_base(const AsNat& x) {
     return x;
 }
 
-int u2(int, int y) {
-    return y;
-}
-
-int r(int n, int init_value, int (*base)(int), int (*r_step)(int, int)) {
-    if (n == 0) {
-        return base(init_value);
-    }
-    return r_step(init_value, r(n-1, init_value, base, r_step));
-}
-
-int add_base(int x) {
-    return x;
-}
-
-int add_r_step(int, int y) {
-    return n(y);
-}
-
-int add(int x, int y) {
-    return r(y, x, add_base, add_r_step);
-}
-
-int multiply_base(int) {
-    return 0;
-}
-
-int multiply_r_step(int x, int y) {
-    return add(x, y);
-}
-
-int multiply(int x, int y) {
-    return r(y, x, multiply_base, multiply_r_step);
-}
-
-int sub_base(int x) {
-    return x;
-}
-
-int sub_r_step(int x, int y) {
+AsNat sub_r_step(const AsNat& x, const AsNat& y) {
     return pred(y);
 }
 
-int sub(int x, int y) {
+AsNat sub(const AsNat& x, const AsNat& y) {
     return r(y, x, sub_base, sub_r_step);
 }
 
 
+AsNat add_base(const AsNat& x) {
+    return x;
+}
+
+
+AsNat add_r_step(const AsNat&, const AsNat& y) {
+    return n_func(y);
+}
+
+
+AsNat add(const AsNat& x, const AsNat& y) {
+    return r(y, x, add_base, add_r_step);
+}
+
+
+AsNat multiply_base(const AsNat&) {
+    return AsNat(0);
+}
+
+AsNat multiply_r_step(const AsNat& x, const AsNat& y) {
+    return add(x, y);
+}
+
+AsNat multiply(const AsNat& x, const AsNat& y) {
+    return r(y, x, multiply_base, multiply_r_step);
+}
+
 int main() {
-    std::cout<<multiply(25, 5)<<" ";
-    std::cout<<sub(108,101)<<" ";
+    AsNat m = multiply(AsNat(25), AsNat(5));
+    std::cout << "25 * 5 = " << m << " (Ожидается 125)\n";
+
+    AsNat s = sub(AsNat(108), AsNat(101));
+    std::cout << "108 - 101 = " << s << " (Ожидается 7)\n";
+
+    std::cout << "pred(0) = " << pred(AsNat(0)) << " (Ожидается 0)\n";
+    std::cout << "pred(1) = " << pred(AsNat(1)) << " (Ожидается 0)\n";
+    std::cout << "pred(2) = " << pred(AsNat(2)) << " (Ожидается 1)\n";
+    std::cout << "pred(3) = " << pred(AsNat(3)) << " (Ожидается 2)\n";
+
+    std::cout << "sub(10, 5) = " << sub(AsNat(10), AsNat(5)) << " (Ожидается 5)\n";
+    std::cout << "sub(5, 10) = " << sub(AsNat(5), AsNat(10)) << " (Ожидается 0)\n";
+    std::cout << "sub(7, 7) = " << sub(AsNat(7), AsNat(7)) << " (Ожидается 0)\n";
+
+    return 0;
 }
